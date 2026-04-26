0d64d418960f03061471d9e748824e2e477ff50d
Amir-Battal <ameerbatttal@hotmail.com>
2026-04-26 15:07:06 +0300

    Change website Icon + mock photos

--- src/components/initiative/volunteerManagement/UserCard.tsx
+++ src/components/initiative/volunteerManagement/UserCard.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import type { Volunteer } from '../../../schemas/volunteerSectionSchema';
 import { AttendanceCalendar } from './AttendanceCalendar';
+import { MailIcon, Phone } from 'lucide-react';
 
 interface Props {
     volunteer: Volunteer;
@@ -29,17 +30,16 @@ const UserCard = ({volunteer}: Props) => {
           <div className="feature-grid">
             <div className="feature-item">
               <div className="feature-icon">
-                <svg viewBox="0 0 24 24">
+                {/* <svg viewBox="0 0 24 24">
                   <path d="M20,4C21.1,4 22,4.9 22,6V18C22,19.1 21.1,20 20,20H4C2.9,20 2,19.1 2,18V6C2,4.9 2.9,4 4,4H20M4,6V18H20V6H4M6,9H18V11H6V9M6,13H16V15H6V13Z" />
-                </svg>
+                </svg> */}
+                <MailIcon />
               </div>
               <span className="feature-text">{volunteer.email}</span>
             </div>
             <div className="feature-item">
               <div className="feature-icon">
-                <svg viewBox="0 0 24 24">
-                  <path d="M12,17.56L16.07,16.43L16.62,10.33H9.38L9.2,8.3H16.8L17,6.31H7L7.56,12.32H14.45L14.22,14.9L12,15.5L9.78,14.9L9.64,13.24H7.64L7.93,16.43L12,17.56M4.07,3H19.93L18.5,19.2L12,21L5.5,19.2L4.07,3Z" />
-                </svg>
+                <Phone />
               </div>
               <span className="feature-text"><h4>{volunteer.phoneNumber}</h4></span>
             </div>
