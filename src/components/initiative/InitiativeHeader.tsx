d28028929bd98079719d6f38e24a8e844cc45e74
Amir-Battal <ameerbatttal@hotmail.com>
2026-05-22 05:49:24 +0300

    Incomplete profile message warning

--- src/components/initiative/InitiativeHeader.tsx
+++ src/components/initiative/InitiativeHeader.tsx
@@ -1,6 +1,9 @@
 import NewInitiativeOverlay from "./NewInitiativeOverlay";
 import Filters, { type FiltersType } from "./Filters.tsx";
 import { getUserRole } from "../../lib/utils.ts";
+import { useGetMyUser } from "../../hooks/use-user.ts";
+import { MessageSquareWarningIcon } from "lucide-react";
+import { useNavigate } from "react-router";
 
 interface InitiativeHeaderProps {
     onFiltersChange: (filters: FiltersType) => void;
@@ -8,13 +11,49 @@ interface InitiativeHeaderProps {
 
 const InitiativeHeader = ({ onFiltersChange }: InitiativeHeaderProps) => {
     const userRole = getUserRole();
+    const currentUser = useGetMyUser().currentUser;
 
+    const navigate = useNavigate();
+
+    const isProfileIncomplete =
+        !currentUser?.academicYear ||
+        !currentUser?.birthdate ||
+        !currentUser?.collegeName ||
+        !currentUser?.description ||
+        !currentUser?.location ||
+        !currentUser?.phone ||
+        !currentUser?.studentNumber;
+    
     return (
-        <div className="flex w-full justify-start items-center gap-5 pl-10">
-            {(userRole === "Admin" || userRole === "User") && (
-                <NewInitiativeOverlay/>
+        <div className="flex flex-col w-full justify-start items-center gap-5 pl-10">
+            <div className="w-full flex flex-row gap-5">
+                {(userRole === "Admin" || userRole === "User") && (!isProfileIncomplete) && (
+                    <NewInitiativeOverlay/>
+                )}
+
+                <Filters onFiltersChange={onFiltersChange} />
+            </div>
+
+            {isProfileIncomplete && (
+                <div className="flex h-[4rem] w-full flex-row items-center justify-center gap-5 rounded-xl bg-black text-white">
+                    <MessageSquareWarningIcon />
+
+                    <h1>
+                    يجب عليك إكمال بيانات{" "}
+                    
+                    <span
+                        className="cursor-pointer font-bold underline text-red-300 hover:text-red-500"
+                        onClick={() => {
+                            navigate(`/profile/${currentUser?.userId}`);
+                        }}
+                    >
+                        حسابك الشخصي
+                    </span>
+
+                    {" "}لكي تتمكن من إنشاء مبادرة أو المشاركة في الأنشطة التطوعية
+                    </h1>
+                </div>
             )}
-            <Filters onFiltersChange={onFiltersChange} />
         </div>
     );
 };
