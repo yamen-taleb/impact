324cc2fd0b99464046116dc3f4d482555c01415a
Amir-Battal <ameerbatttal@hotmail.com>
2026-08-18 17:22:25 +0300

    Add roles line on NAVBAR

--- src/components/AuthenticatedHeader.tsx
+++ src/components/AuthenticatedHeader.tsx
@@ -1,11 +1,21 @@
 import HeaderAvatar from "./HeaderAvatar.tsx";
 import Navbar from "./Navbar.tsx";
 import AppTittle from "./AppTitle.tsx";
+import { useUserContext } from "../context/UserContext.tsx";
+import { getUserRole } from "../lib/utils.ts";
 
 const AuthenticatedHeader = () => {
+    const role = getUserRole();
 
     return (
-        <header className="sticky top-0 z-50 border-b border-zinc-200 bg-white shadow-sm">
+        <header className={`sticky top-0 z-50 border-b-2 bg-white shadow-sm ${
+                (role === "User") 
+                ? "border-black" 
+                : (role === "Admin")
+                ? "border-green-600"
+                : "border-red-600"
+            }`}
+        >
             <div className="mx-32 flex items-center justify-between px-4 py-3 sm:px-6">
                 <AppTittle/>
                 <Navbar/>
